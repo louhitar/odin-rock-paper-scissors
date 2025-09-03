b5ffec6bef97fe8e769a952a00a20ec44a9f3369
louhitar <199230277+louhitar@users.noreply.github.com>
2025-09-03 16:19:37 +0300

    Create and implement logic for computer choice

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hi there!")
+// SET variable computerChoice to ""
+let computerChoice = getComputerChoice();
+
+function getComputerChoice() {
+    // Generate a random number of 0, 1, or 2
+    let randomNum = Math.floor(Math.random() * 3);
+    // Assign randomNum rock/paper/scissors based on generated number
+    let decision = "";
+    switch (randomNum) {
+        case 0:
+            decision = "Rock";
+            break;
+        case 1:
+            decision = "Paper";
+            break;
+        case 2:
+            decision = "Scissors";
+            break;
+    }
+    return decision;
+}
